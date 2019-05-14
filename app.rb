6676546d212670162cc71c0d075416f177717fac
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-05-14 22:43:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/:name' do 
+    @name = [:name]
+    "My name is #{name} "
+  end 
+  
+  
+  get '/:hometown' do 
+    @hometown = [:hometown]
+    "My hometown is #{hometown} "
+  end 
+  
+  
+  get '/:favorite_song' do 
+    @favorite_song = [:favorite_song]
+    "My favorite song is #{favorite_song} "
+  end 
+  
+  
 end
